dab87fc0d1d347afb88f7e33ec05c2ece5954645
Alex Kofford <alexkofford@gmail.com>
2018-09-27 14:45:03 -0600

    Fixed scanner camera on phone

--- Assets/Code/Follow.cs
+++ Assets/Code/Follow.cs
@@ -13,8 +13,9 @@ public class Follow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.position = camera.transform.position;
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
         transform.rotation = Quaternion.Euler(90 + camera.transform.rotation.eulerAngles.x,
-            camera.transform.rotation.eulerAngles.y,
+            camera.transform.rotation.eulerAngles.y + 180,
             camera.transform.rotation.eulerAngles.z);
 	}
 }
